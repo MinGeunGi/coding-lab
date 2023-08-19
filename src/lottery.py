3b58c27e92f34832fbeb190f9de83ef17e24225e
MinGeunGi <coolcool0518@naver.com>
2023-08-19 12:02:03 +0900

    changed the code

--- src/lottery.py
+++ src/lottery.py
@@ -1,26 +1,41 @@
 import random
 
-lotto_numbers = []
 
 
-value = random.randint(1, 50)
+  
 
 
-lotto_numbers = []
-my_num = 1
-is_ok = True
-while is_ok:
-  
-  value = random.randint(1, 50)
-  
-  if value in lotto_numbers:
-    continue
-  lotto_numbers.append(value)
-  if len(lotto_numbers) == 6:
-    break
+
+
+
+
+
+
+
+
+
   
   
-print(lotto_numbers)
+def lotto():
+  lotto_numbers = []
+  is_ok = True
+  while is_ok:
+    
+    value = random.randint(1, 50)
+  
+    if value in lotto_numbers:
+      continue
+    lotto_numbers.append(value)
+    if len(lotto_numbers) == 6:
+      break
+  return lotto_numbers
+
+result = lotto()
+
+print(result)
+  
+  
+
   
 
 
--- src/munje.py
+++ src/munje.py
@@ -1,11 +1,31 @@
 
-class M:
-  def __init__(self, a):
-    self.a = input()
-  
- 
-  
+def add(a, b, works):
+
+  if works:
+    return a + b
+
+def a(a, b):
+#뺄
+  return a - b
+
+def b(a, b):
+#곱
+  return a * b
+
+def test_list():
+  my_list = [1, 2, 3, 4]
+  return my_list
 
 
-for j in range():
-  print('*'*j)
+
+
+
+
+result = add(1, 11, True)
+list_result = test_list(1)
+print(list_result)
+
+
+
+#for j in range(a):
+  #print('*'*j)
