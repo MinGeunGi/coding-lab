bc2fa017411710e1c337cceecfaf73c1590c8dd2
MinGeunGi <coolcool0518@naver.com>
2023-11-04 11:53:42 +0900

    학생성적 데이터 베이스 입력

--- src/DB/12_score_mansger/DB.work.py
+++ src/DB/12_score_mansger/DB.work.py
@@ -3,6 +3,7 @@ import sqlite3
 
 con = sqlite3.connect("scoer.db")
 cur = con.cursor()
+#score테이블 - name(학생이름), korean(국어), math(수학점수), english(영어 점수), kind(M중간 / F기말)
 cur.execute("CREATE TABLE score(name, korean, math, english, kind)")
 
 con.close()
--- src/DB/12_score_mansger/main.py
+++ src/DB/12_score_mansger/main.py
@@ -1,3 +1,4 @@
+import sqlite3
 '''
 기능 명세
 1. 학생 중간/기말 평균
@@ -25,24 +26,53 @@ def input_score():
     학생이름 국어, 수학, 영어 구분
 
     '''
-    pass
+    print("======학생성적 입력======")
+    print("학생이름, 국어, 수학, 영어, 중간(M)/기말(F) 구분 순서대로 입력 해주세요")
+    print("예시 : 홍길동 98 85 95 M")
+    input_value = input().split(' ')
+    print(input_value)
+    
+    if len(input_value) != 5:
+        print('다시 입력해주세요')
+        return input_score
+    
+    if (input_score[4] != 'M') or (input_score[4] != 'F'):
+        print('중간 기말 구분 해주세요')
+        return 
+    
+    con = sqlite3.connect("scoer.db")
+    cur = con.cursor()
+    cur.execute(
+        f'INSERT INTO score VALUES('{input_value[0]}', '{input_value[1]}', '{input_value[2]}', '{input_value[3]}', '{input_value[4]})'
+    )
+    con.commit()
+    con.close()
 
 
-print_menu()
-is_working = True
-while (is_working):
 
-    Exit = input('원하시는 메뉴를 선택 해주세요 :')
 
-    if Exit == "exit":
-        print("프로그램을 종료 합니다 :D")
-        is_working = False
-    if Exit == '1':
-        print('1. 학생성적 입력 \n')
-    if Exit == '2':
-        print('2. 한 학생의 성적 출력 \n')
-    if Exit == '3':
-        print('3. 평균 보여주기 \n')
-    if Exit == '4':
-        print_menu()
-        print('\n\n')
+def run():  # 프로그램의 시작점
+    print_menu()
+    is_working = True
+    while (is_working):
+
+        Exit = input('원하시는 메뉴를 선택 해주세요 :')
+
+        if Exit == "exit":
+            print("프로그램을 종료 합니다 :D")
+            is_working = False
+        if Exit == '1':
+            print('1. 학생성적 입력 \n')
+            input_score()
+            
+        if Exit == '2':
+            print('2. 한 학생의 성적 출력 \n')
+        if Exit == '3':
+            print('3. 평균 보여주기 \n')
+        if Exit == '4':
+            print_menu()
+            print('\n\n')
+
+
+run()
+input_score()
