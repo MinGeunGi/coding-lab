02e13b4607fc73a18ee936929720cc2b2ba0d8fb
MinGeunGi <coolcool0518@naver.com>
2023-11-18 11:49:29 +0900

    입력 버그 수정 및 데이터 베이스 사용

--- src/DB/12_score_mansger/main.py
+++ src/DB/12_score_mansger/main.py
@@ -31,24 +31,32 @@ def input_score():
     print("예시 : 홍길동 98 85 95 M")
     input_value = input().split(' ')
     print(input_value)
-    
+
     if len(input_value) != 5:
         print('다시 입력해주세요')
         return input_score
-    
-    if (input_score[4] != 'M') or (input_score[4] != 'F'):
+
+    if input_value[4] not in ["M", "F"]:
         print('중간 기말 구분 해주세요')
-        return 
-    
+        return
+
     con = sqlite3.connect("scoer.db")
     cur = con.cursor()
     cur.execute(
-        f'INSERT INTO score VALUES('{input_value[0]}', '{input_value[1]}', '{input_value[2]}', '{input_value[3]}', '{input_value[4]})'
+        f"INSERT INTO score VALUES('{input_value[0]}', '{input_value[1]}', '{input_value[2]}', '{input_value[3]}', '{input_value[4]}')"
     )
     con.commit()
     con.close()
 
 
+def show_score():
+    # 학생성적 조회
+    con = sqlite3.connect("scoer.db")
+    cur = con.cursor()
+    result = cur.execute("SELECT name, korean, math, english, kind FROM score")
+    score = result.fetchall()
+    for data in score:
+        print('이름:', data[0],',', '국어:', data[1],',', '수학:', data[2],',', '영어:', data[3],',', 'M/F:', data[4])
 
 
 def run():  # 프로그램의 시작점
@@ -64,9 +72,10 @@ def run():  # 프로그램의 시작점
         if Exit == '1':
             print('1. 학생성적 입력 \n')
             input_score()
-            
+
         if Exit == '2':
             print('2. 한 학생의 성적 출력 \n')
+            show_score()
         if Exit == '3':
             print('3. 평균 보여주기 \n')
         if Exit == '4':
@@ -76,3 +85,4 @@ def run():  # 프로그램의 시작점
 
 run()
 input_score()
+show_score()
